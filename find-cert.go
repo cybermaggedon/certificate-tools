d9a86407b500751dc265afb72d7c759b778b4e05
Greg M <greg.molchany@trustnetworks.com>
2018-05-18 09:08:47 -0400

    Changed Subject.string access method [ch2083]

--- find-cert.go
+++ find-cert.go
@@ -82,7 +82,9 @@ func main() {
 			}
 
 			// Temporarily hold the Subject string from the certificate
-			tmpSubject := testCert.Subject.String()
+			//tmpSubject := testCert.Subject.String()
+			tmpSubject := testCert.Subject.ToRDNSequence().String()
+			
 
 			// Look through the array of email addresses in the Cert for a Match
 			// then optionally look for the provided Subject substring in the Cert Subject field
